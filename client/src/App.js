93b0596a4c44a3db23fb7e07f33f1a429c9c3e65
Hope-Ray <anuragshetty007@gmail.com>
2023-01-04 22:56:47 +0530

    Changes 2
    test 2

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,31 @@ import React from "react";
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 import tourister from './images/tourister.png'
 
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
+import useStyles from "./styles";
+
 const App = () => {
+    const classes = useStyles();
+
     return (
         <Container maxidth="lg">
-            <AppBar position="static" color="inherit">
-                <Typography variant="h2" align ="center">Tourister</Typography>
-                <img src={tourister} alt="tourister" height="60" width={60}></img>
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography className={classes.heading} variant="h2" align ="center">Tourister</Typography>
+                <img className={classes.image} src={tourister} alt="tourister" height="60"></img>
             </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts/>
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form/>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
         </Container>
     );
 }
